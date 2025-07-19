93186818fa0843911385a5639dae09323d123031
ruebeniosdev <akankobatengr@gmail.com>
2025-07-19 14:53:33 +0000

    had errors

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,8 +1,21 @@
+import { useNavigate } from "@tanstack/react-router";
+import { useCallback } from "react";
 import { pb } from "@/lib/PocketBase";
 
-export default function useLogout() {
-  function logout() {
+export function useAuth() {
+  const navigate = useNavigate();
+
+  const isLoggedIn = !!pb.authStore.token;
+  const user = pb.authStore.record; 
+
+  const logout = useCallback(() => {
     pb.authStore.clear();
-  }
-  return logout;
+    navigate({ to: "/login" });
+  }, [navigate]);
+
+  return {
+    isLoggedIn,
+    user,
+    logout,
+  };
 }
