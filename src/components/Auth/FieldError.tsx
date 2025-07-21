e2d990f248be6d9ffb4eb0d4ab08ee7c03eac9ab
ruebeniosdev <akankobatengr@gmail.com>
2025-07-21 21:48:07 +0000

    fix: typescript errors

--- src/components/Auth/FieldError.tsx
+++ src/components/Auth/FieldError.tsx
@@ -1,2 +1,7 @@
-export const FieldError = ({ error }: { error?: string }) =>
-  error ? <div className="text-red-500 text-sm">{error}</div> : null;
+export const FieldError = ({ error }: { error?: string }) => {
+  return (
+    <div>
+      error ? <div className="text-red-500 text-sm">{error}</div> : null;
+    </div>
+  );
+};
